bd3d3b28903237ba81a91b27e4a799c3247912c8
Raihan Rasheed Apurbo <apurbo97@gmail.com>
2021-06-03 10:32:02 +0600

    memory efficient lcs path completed

--- memoryEfficientLCSPath.cpp
+++ memoryEfficientLCSPath.cpp
@@ -1,5 +1,93 @@
 #include<bits/stdc++.h>
 using namespace std;
+enum direction{RIGHT,BOTTOM,DIAGONAL};
+int lcsForTrivial(string a, string b)
+{
+    vector<vector<int>> s(a.size()+1);
+    vector<vector<int>> p(a.size()+1);
+    for(int i=0;i<s.size();i++)
+    {
+        vector<int> t(b.size()+1);
+        for(int j=0;j<t.size();j++)
+        {
+            t[j] = -5;
+        }
+        
+        s[i] = t;
+        p[i] = t;
+    }
+    // initializing first row and first
+    // column with boundary condition which
+    // is in this case 0
+    for(int i=0;i<s.size();i++)
+    {
+        s[i][0] = 0;
+    }
+    for(int i=0;i<s[0].size();i++)
+    {
+        s[0][i] = 0;
+    }
+
+    for(int i=1;i<s.size();i++)
+    {
+        for(int j=1;j<s[0].size();j++)
+        {
+            if(a[i-1]==b[j-1])
+            {
+                s[i][j] = s[i-1][j-1] + 1;
+                p[i][j] = DIAGONAL; 
+            }
+            else if(s[i-1][j]>s[i][j-1])
+            {
+                s[i][j] = s[i-1][j];
+                p[i][j] = BOTTOM; 
+            }
+            else
+            {
+                s[i][j] = s[i][j-1];
+                p[i][j] = RIGHT;
+            }
+        }
+    }
+    //acquiring reverse of the lcs by
+    //traversing the path table backwards
+    int ci = a.size();
+    int cj = b.size();
+    string reverseLcs = "";
+    while(p[ci][cj] != -5)
+    {
+        if(p[ci][cj] == BOTTOM)
+        {
+            ci -= 1;
+        }
+        else if(p[ci][cj] == RIGHT)
+        {
+            cj -= 1;
+        }
+        else if(p[ci][cj] == DIAGONAL)
+        {
+            reverseLcs += a[ci-1];
+            ci -= 1;
+            cj -= 1;
+        }
+
+
+    }
+
+
+
+    // generating reverse string of reverseLcs
+    // which is our desired lcs
+    string lcs = "";
+    for(int i=reverseLcs.size()-1;i>=0;i--)
+    {
+        lcs += reverseLcs[i];
+    }
+    //printing both
+    // cout<<reverseLcs<<endl;
+    cout<<lcs;
+}
+
 
 int lcsLengthMemoryEfficient(string a, string b)
 {
@@ -62,43 +150,46 @@ void lcsPathMemoryEfficient(string a, string b, int leftA, int rightA, int leftB
     //6 A n B B R D R D     //6 A 0 1 2 2 3 3 4
     //7 T n D B R B D R     //7 T 0 1 2 2 3 4 4
 
-    if(leftB==rightB)
+    if(abs(leftB-rightB)<=2)
     {
-        int maxLCS = -1;
-        int maxIndex = -1;
-        for(int i=1;i<a.size()+1;i++)
-        {
-            string rowString = a.substr(0,i);
-            string colString = b.substr(left,left+1);
-            int t = lcsLengthMemoryEfficient(rowString,colString);
-            if(t>=maxLCS)
-            {
-                maxIndex = i;
-            } 
-        }
-        cout<<maxIndex<<","<<left<<endl;
+        string rowString = a.substr(leftA,rightA-leftA+1);
+        string colString = b.substr(leftB,rightB-leftB+1);
+        lcsForTrivial(rowString,colString);
+        return;
+        // int maxLCS = -1;
+        // int maxIndex = -1;
+        // for(int i=1;i<a.size()+1;i++)
+        // {
+            
+        //     int t = lcsLengthMemoryEfficient(rowString,colString);
+        //     if(t>=maxLCS)
+        //     {
+        //         maxIndex = i;
+        //     } 
+        // }
+        // cout<<maxIndex<<","<<left<<endl;
         return;
     }
     int maxLCS = -1;
     int maxIndex = -1;
     int midColumn = (leftB+rightB)/2;
-    for(int i=leftA+1;i<rightA+2;i++)
+    for(int i=1;i<=rightA-leftA+1;i++)
     {
         string rowString = a.substr(leftA,i);
-        string colString = b.substr(leftB,midColumn);
+        string colString = b.substr(leftB,midColumn-leftB+1);
         //cout<<rowString<<" "<<colString<<endl;
         int t1 = lcsLengthMemoryEfficient(rowString,colString);
         //cout<<t1<<endl;
         
-        rowString = a.substr(i,rightA+1);
-        colString = b.substr(midColumn,rightB+1);
+        rowString = a.substr(leftA+i,rightA-leftA-i+1);
+        colString = b.substr(midColumn+1,rightB-midColumn-1+1);
         int t2 = lcsLengthMemoryEfficient(rowString,colString);
         //cout<<t2<<endl;
         //cout<<t1+t2<<endl;
         if(maxLCS<=t1+t2)
         {
             maxLCS = t1+t2;
-            maxIndex = i;
+            maxIndex = leftA+i-1;
         }
     }
     
@@ -110,8 +201,15 @@ void lcsPathMemoryEfficient(string a, string b, int leftA, int rightA, int leftB
 
 int main()
 {
-    string a = "ATCTGAT";
-    string b = "TGCATA";
-    lcsPathMemoryEfficient(a,b,0,b.size());
+    string a = "ATCTGATATCTGAT";
+    string b = "TGCATATGCATA";
+    cout<<"lcs trivial"<<endl;
+    lcsForTrivial(a,b);
+    cout<<endl;
+    cout<<"lcs memory efficient"<<endl;
+    lcsPathMemoryEfficient(a,b,0,a.size()-1,0,b.size()-1);
+    cout<<endl;
+    // cout<<a.substr(3,7);
+
     
 }
